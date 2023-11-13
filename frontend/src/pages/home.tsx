14fa8325ada74ad6cbe9a76389d8ffe53e418b06
Valentin Reault <valentin.reault@etu.univ-orleans.fr>
2023-11-13 21:21:34 +0100

    update home css

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,3 +1,4 @@
+
 export default function Home() {
     return (
         <div style={{
@@ -5,12 +6,25 @@ export default function Home() {
             backgroundSize: "cover",
             height: "100vh"
             }}>
-            <img src="logoV1.png" alt="logo" style={{
-                width: "20vw"
-                }}
-            />
-            <button type="submit">Connexion</button>
-            <a href="">Pas encore de compte</a>
+            <div style={{paddingLeft: "30vw",
+                            paddingTop: "5%",
+                            textAlign:"center"}}>
+                <img src="logoV1.png" alt="logo" style={{
+                    width: "20vw",
+                    margin:"auto",
+                    display:"block"
+                    }}
+                />
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" style={{
+                    marginTop: "2em"
+                    }}
+                >Connexion</button>
+                <br />
+                <a href="" style={{
+                    marginTop: "2em"
+                    }}>Pas encore de compte ?</a>
+            </div>
+            
           </div>
     )    
   }
