1b518fb4ec79fef6e0706fd1bd55dd8b939d3d3a
Maxime Bourdon <mbourdon.pro@gmail.com>
2023-11-15 18:47:32 +0100

    Add Update Function in User Route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -3,17 +3,19 @@ import User from '../models/user.model'
 import UserGroup from '../models/user_group.model'
 import validator from 'validator'
 import Group from '../models/group.model'
-import { failRequest, isUserIdFromTokenMatchingRequest } from '../utils/functions'
+import { failRequest, isUserIdFromTokenMatchingRequest, hashPassword } from '../utils/functions'
+
+
 
 const routerUser: Router = express.Router()
 
 routerUser.get('/:id', getUserById)
 routerUser.get('/:id/groups', getUserGroupsById)
+routerUser.put('/:id', updateUserById)
 
 export default routerUser
 
 async function getUserById(req: Request, res: Response): Promise<void> {
-
     try {
         const id = Number(req.params.id)
         
@@ -53,5 +55,37 @@ async function getUserGroupsById(req: Request, res: Response): Promise<void> {
     } catch (error) {
         console.error(error)
         failRequest(res,500,`Internal server error`)
+    }
+}
+
+async function updateUserById(req: Request, res: Response): Promise<void> {
+    try {
+        const defaultFail = (): void => failRequest(res, 401, `Incorrect email`)
+        // I'm trying to find a solution tu re-use the fonction getuserById
+        const id: number = Number(req.params.id);
+
+        if (!isUserIdFromTokenMatchingRequest(req.headers.authorization, id))
+            return failRequest(res, 401, `Unauthorized`);
+
+        if (!validator.isEmail(req.body.email))
+            return defaultFail();
+
+        const updatedUser = await User.update(
+            {   
+                nickname: req.body.nickname, 
+                email: req.body.email,
+                password:await hashPassword(req.body.password)
+
+            },
+            { 
+                where: { id }, 
+                returning: true 
+            }
+        );
+
+        res.json(updatedUser);
+    } catch (error) {
+        console.error(error)
+        failRequest(res, 500, `Internal server error`)
     }
 }
