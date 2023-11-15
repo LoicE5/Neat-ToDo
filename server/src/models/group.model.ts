50a2dc55d227c2d7258ee6c6697173896dade506
Loïc <59501884+LoicE5@users.noreply.github.com>
2023-11-15 11:56:25 +0100

    Models update and simplification

--- server/src/models/group.model.ts
+++ server/src/models/group.model.ts
@@ -1,20 +1,25 @@
 import sequelize from "../../db"
-import { DataTypes } from "sequelize"
+import {
+    DataTypes
+} from "sequelize"
 
 const Group = sequelize.define('Group', {
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     name: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        validate: {
+            isAlphanumeric: true
+        }
     },
-  }, {
-    tableName: 'group', // Adjust the table name if needed
+}, {
+    tableName: 'group',
     timestamps: false,
 })
-  
+
 export default Group
--- server/src/models/todoom.model.ts
+++ server/src/models/todoom.model.ts
@@ -1,54 +1,66 @@
 import sequelize from "../../db"
-import { DataTypes } from "sequelize"
+import {
+    DataTypes
+} from "sequelize"
 
 const Todoom = sequelize.define('Todoom', {
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
     group_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'Group',
-        key: 'id',
-      },
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Group',
+            key: 'id',
+        },
+        validate: {
+            isAlphanumeric: true
+        }
     },
     title: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
+        type: DataTypes.STRING(50),
+        allowNull: false,
     },
     description: {
-      type: DataTypes.STRING(500),
+        type: DataTypes.STRING(500),
+
     },
     deadline: {
-      type: DataTypes.DATE,
+        type: DataTypes.DATE,
     },
     status: {
-      type: DataTypes.STRING(30),
-      defaultValue: 'not_started',
-      allowNull: false,
+        type: DataTypes.STRING(30),
+        defaultValue: 'not_started',
+        allowNull: false,
     },
     assignee_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'User',
-        key: 'id',
-      },
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'User',
+            key: 'id',
+        },
+        validate: {
+            isAlphanumeric: true
+        }
     },
     author_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'User',
-        key: 'id',
-      },
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'User',
+            key: 'id',
+        },
+        validate: {
+            isAlphanumeric: true
+        }
     },
-  }, {
+}, {
     tableName: 'todoom',
     timestamps: true,
-  })  
+})
 
 export default Todoom
