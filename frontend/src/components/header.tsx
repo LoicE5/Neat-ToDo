aa7ba425b3185ee7a8063514d01de168512a21fc
Valentin Reault <59821128+ValReault@users.noreply.github.com>
2023-11-18 23:36:11 +0100

    V1 ToDoom component and calls in workplace

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -2,7 +2,7 @@ export default function Header(props:any){
     return(
         <header className="bg-gray-800">
   <nav className="mx-auto flex max-w-7xl items-center justify-between p-2 lg:px-8" aria-label="Global">
-    <div className="flex lg:flex-1">
+    <div className="flex lg:flex-1" >
       <a href="#" className="-m-1.5 p-1.5">
         <img className="h-8 w-auto" src="logoV1.png" alt="" style={{ display: "block", height: "80px" }} />
       </a>
--- frontend/src/pages/workplace.tsx
+++ frontend/src/pages/workplace.tsx
@@ -1,11 +1,23 @@
 import Header from "@/components/header";
+import ToDoom from "@/components/todoom";
 
 export default function Workplace() {
-    return (<div>
+    return (
+    <div>
         <Header/>
         <br />
         <br />
         <h1 style={{ fontWeight: 'bold', fontSize: '1.5em', marginLeft: '20px' }}>Vos ToDoom Perso</h1>
+        <br />
+        <ToDoom title="Acheter des cadeaux de Noël" status="En cours" 
+        description="Faire les achats de Noël parce que là ça commence à être urgent" author="ValReault" deadline="2022-11-12"/>
+
+        <ToDoom title="Faire les courses" status="Pas commencé" 
+        description="Ya plus rien dans le frigo, faut acheter des pates" author="ValReault" deadline="2023-11-23"/>
+
+        <ToDoom title="Finir le projet ToDoom" status="Terminé" 
+        description="Finir ce super projet pour Dauphine avant début décembre" author="ValReault" deadline="2023-12-01"/>
+
     </div>
     
     )
