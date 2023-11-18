3719f6c2487e27864cb12c3d63dc5a992ae7acee
Valentin Reault <59821128+ValReault@users.noreply.github.com>
2023-11-18 21:22:02 +0100

    create workplace and login, header component

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,28 @@
+export default function Header(props:any){
+    return(
+        <header className="bg-gray-800">
+  <nav className="mx-auto flex max-w-7xl items-center justify-between p-2 lg:px-8" aria-label="Global">
+    <div className="flex lg:flex-1">
+      <a href="#" className="-m-1.5 p-1.5">
+        <img className="h-8 w-auto" src="logoV1.png" alt="" style={{ display: "block", height: "80px" }} />
+      </a>
+    </div>
+    <div className="flex lg:hidden">
+      <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+        <span className="sr-only">Open main menu</span>
+        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+    </div>
+    <div className="hidden lg:flex lg:gap-x-12">
+    <a href="#" className="text-lg font-semibold leading-6 text-red-500 no-underline">Créer une ToDoom</a>
+    <a href="#" className="text-lg font-semibold leading-6 text-red-500 no-underline">Vos groupes</a>
+    </div>
+    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+    <a href="#" className="text-lg font-semibold leading-6 text-red-500 no-underline">Log out</a>
+    </div>
+  </nav>
+</header>
+    )
+}
---
+++ frontend/src/pages/workplace.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+
+export default function Workplace() {
+    return (<div>
+        <Header/>
+        <br />
+        <br />
+        <h1 style={{ fontWeight: 'bold', fontSize: '1.5em', marginLeft: '20px' }}>Vos ToDoom Perso</h1>
+    </div>
+    
+    )
+        
+  }
