bea1005d39f828df32c684ce42ddea2967207579
Loïc <59501884+LoicE5@users.noreply.github.com>
2023-11-23 18:14:18 +0100

    Beautify frontend

--- frontend/src/components/todoom.tsx
+++ frontend/src/components/todoom.tsx
@@ -1,45 +1,57 @@
-export default function ToDoom(props:any){
+interface ToDoomProps {
+    deadline: Date | string,
+    status: string,
+    title: string,
+    description: string,
+    author: string
+}
+
+export default function ToDoom({ deadline, status, title, description, author }: ToDoomProps) {
     let showStatus;
-    switch (props.status) {
+    switch (status) {
         case "Pas commencé":
-            showStatus = <p className="ml-auto italic text-white">Pas commencé</p>;
+            showStatus = <p className="ml-auto italic text-white">Pas commencé</p>
             break;
-        
+
         case "En cours":
-            showStatus = <p className="ml-auto italic text-blue-300">En cours</p>;
+            showStatus = <p className="ml-auto italic text-blue-300">En cours</p>
             break;
-        
+
         case "Terminé":
-            showStatus = <p className="ml-auto italic text-green-500">Terminé</p>;
+            showStatus = <p className="ml-auto italic text-green-500">Terminé</p>
             break;
         default:
-            showStatus = <p className="ml-auto italic text-white">Statut inconnu</p>;
+            showStatus = <p className="ml-auto italic text-white">Statut inconnu</p>
             break;
     }
-    let deadline = new Date(props.deadline)
+    
+    if (!(deadline instanceof Date))
+        deadline = new Date(deadline)
+
     let todayDate = new Date()
     let late = null
-    if (deadline<todayDate && props.status != "Terminé") {
-        late = <p className="mr-2 font-bold text-red-500">EN RETARD</p>
-    }
 
-    return(
-        <div className="rounded p-4 text-white" style={{margin:"2em", backgroundColor:"#706c6c", borderRadius:"1em"
+    if (deadline < todayDate && status != "Terminé") 
+        late = <p className="mr-2 font-bold text-red-500">EN RETARD</p>
+    
+    return (
+        <div className="rounded p-4 text-white" style={{
+            margin: "2em", backgroundColor: "#706c6c", borderRadius: "1em"
         }}>
             <div className="flex">
-                <h3 className="mr-auto text-lg font-bold">{ props.title }</h3>
+                <h3 className="mr-auto text-lg font-bold">{title}</h3>
                 <div className="flex">
                     {late}
                     {showStatus}
                 </div>
-                
+
             </div>
             <div>
-                <p className="ml-10">{ props.description }</p>
+                <p className="ml-10">{description}</p>
             </div>
             <div className="flex">
-                <p className="mr-auto">{ props.author }</p>
-                <p className="ml-auto">{deadline.getDate()}/{deadline.getMonth()+1}/{deadline.getFullYear()}</p>
+                <p className="mr-auto">{author}</p>
+                <p className="ml-auto">{deadline.getDate()}/{deadline.getMonth() + 1}/{deadline.getFullYear()}</p>
             </div>
         </div>
     )
