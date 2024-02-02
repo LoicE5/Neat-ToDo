51f1ef9798b74a4f3ca33ec398b5e87cddc37ccd
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-02-02 16:20:29 +0100

    Rewrite paths in next.js only in dev env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'http://localhost:3001/api/:path*'
-          },
+        if(process.env.NODE_ENV === 'development')
+            return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3001/api/:path*'
+            },
         ]
     },
 }
