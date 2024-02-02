4368d9a382f0ab1ba0bf646a5700c7d3f879537c
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-02-02 11:15:02 +0100

    Try with __importDefault

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
-const app = require('../server/build/index.js')
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+
+const app = __importDefault(require('../server/build/index.js'))
 
 exports.default = app
