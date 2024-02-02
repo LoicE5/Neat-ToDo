2f1f533b314a54e8b47fb065c218a742b531bb74
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-02-02 16:14:29 +0100

    Use /api prefix for localhost

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:3001/api/:path*'
+          },
+        ]
+    },
 }
 
 module.exports = nextConfig
