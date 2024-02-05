e7e7be24389c117fd523c9e40dfbb183373ae326
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-02-05 16:40:51 +0100

    Try to replace js with ts in api dir

--- api/index.js
+++
@@ -1,7 +0,0 @@
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-
-const app = __importDefault(require('../server/build/index.js'))
-
-exports.default = app
