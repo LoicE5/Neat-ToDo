7ab5a20047e0d5a3ab4c0fcb0b7639bd7e70837a
Loïc <59501884+LoicE5@users.noreply.github.com>
2024-03-19 19:49:14 +0100

    Removed some semicolons

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 // pages/_app.tsx
-import { AppProps } from 'next/app';
+import { AppProps } from 'next/app'
 
-import '@/styles/globals.css';
-import '@/styles/home.css';
-import { AnimatePresence, motion } from 'framer-motion';
+import '@/styles/globals.css'
+import '@/styles/home.css'
+import { AnimatePresence, motion } from 'framer-motion'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
